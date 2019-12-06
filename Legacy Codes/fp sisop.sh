1e4e561fed268dd7fe6856df0c8d9735aa8032a9
fiqq <58363887+fiqq@users.noreply.github.com>
2019-12-06 23:53:33 +0700

    Update fp sisop.sh

--- Legacy Codes/fp sisop.sh
+++ Legacy Codes/fp sisop.sh
@@ -6,9 +6,11 @@ echo "1. KALKULATOR"
 echo "2. PROGAM BERGUNA"
 echo "3. SELESAI"
 read pilih
+clear
 
 case $pilih in
   1 )
+    loop=1
     while [[ loop -eq 1 ]]; do
     echo "1.PENJUMLAHAN"
     echo "2.PENGURANGAN"
@@ -22,38 +24,51 @@ case $pilih in
     read pilih1
       case $pilih1 in
         1 )
+          clear
           echo "INPUTKAN ANGKA PERTAMA :"
           read a
           echo "INPUTKAN ANGKA KEDUA   :"
           read b
           let jumlah=$a+$b
           echo "HASIL PENJULAHAN $a dan $b=$jumlah"
+          read
+          clear
           ;;
         2 )
+          clear
           echo "INPUTKAN ANGKA PERTAMA"
           read a
           echo "INPUTKAN ANGKA KEDUA"
           read b
           let kurang=$a-$b
           echo "HASIL PENGURANGAN $a dan $b = $kurang"
+          read
+          clear
           ;;
         3 )
+          clear
           echo "INPUTKAN ANGKA PERTAMA"
           read a
           echo "INPUTKAN ANGKA KEDUA"
           read b
-          let kali=$a+$b
+          let kali=$a*$b
           echo "HASIL PERKALIAN $a dan $b = $kali"
+          read
+          clear
           ;;
         4 )
+          clear
           echo "INPUTKAN ANGKA PERTAMA"
           read a
           echo "INPUTKAN ANGKA KEDUA"
           read b
           let bagi=$a/$b
           echo "HASIL PEMBAGIAN $a dan $b = $bagi"
+          read
+          clear
           ;;
         5 )
+          clear
           pangkat=1
           echo "INPUTKAN ANGKA YANG YANG MAU DIPANGKATKAN"
           read a
@@ -63,30 +78,38 @@ case $pilih in
             let pangkat=$pangkat*$a
           done
           echo "HASIL PEMANGKATAN $a^$b = $pangkat"
-            ;;
+          read
+          clear
+          ;;
         6 )
+          clear
           echo "INPUTKAN ANGKA YANG YANG MAU DIMODULUS"
           read a
           echo "INPUTKAN MODULUS"
           read b
           let modulus=$a%$b
           echo "HASIL $a MODULUS $b = $modulus"
-            ;;
+          read
+          clear
+          ;;
         7 )
+          clear
           echo "INPUTKAN ANGKA YANG YANG MAU DIAKAR"
           read a
           for (( i = 1; i <=a; i++ )); do
             akar=$i*$i
             if [[ $akar -eq $a ]]; then
               echo "akar dari $a =$i"
-
-          elif [[ $i -eq $a && $akar != $a ]]; then
-              echo "akar tidak ditemukan"
             fi
           done
-            ;;
+          read
+          clear
+          ;;
         8 )
+          clear
           echo "TERIMA KASIH"
+          read
+          clear
           loop=2
       esac
     done
@@ -107,13 +130,16 @@ case $pilih in
 
       case $pilih1 in
         1 )
+          clear
           echo "MAU BIKIN FILE YAA......."
           echo "SILAHKAN MASUKKAN NAMA FILE PLUS FORMAT FILENYA EX |p.txt|"
           read f
           mousepad $f
+          read
           clear
           ;;
         2 )
+          clear
           echo "MAU BUKA FILE ATAU SOFTWARE"
           echo "1.file"
           echo "2.software"
@@ -126,6 +152,8 @@ case $pilih in
               echo "NAMA FILENYA APA"
               read fil
               cat $fil
+              read
+              clear
             fi
             if [[ $pilih2 -eq 2 ]]; then
               clear
@@ -133,10 +161,13 @@ case $pilih in
               echo "NAMA softwarenya APA"
               read fil
               $fil
+              read
+              clear
             fi
             ;;
         3 )
           ls -l
+          clear
           echo "cieee mau merubah hak akses berkas"
           echo "nama filenya apa"
           read fil
@@ -146,8 +177,11 @@ case $pilih in
           chmod $hak $fil
           ls -l $fil
           echo "selesai"
+          read
+          clear
           ;;
         4 )
+          clear
           ls -l
           echo "nama file yang dihapus"
           read fil
@@ -155,29 +189,44 @@ case $pilih in
           rm  $fil
           ls -l $fil
           echo "yaaaaaa file anda sudah terhapus"
+          read
+          clear
             ;;
         5 )
+          clear
           ls -l
           echo "MAU NGEDIT FILE APA :"
           read fil
           mousepad $fil
           echo "selesai"
+          read
+          clear
           ;;
         6 )
+          clear
           echo "APAKAH ANDA TERSAMBUNG INTERNET"
           echo "1.ya"
           echo "2.tidak"
           read net
           if [[ $net -eq 1 ]]; then
+            clear
             echo "MASUKKAN URL ATAU LINK FILE YANG INGIN DI DOWNLOAD"
             read url
             wget $url
+            read
+            clear
           fi
           if [[ $net -eq 2 ]]; then
             echo "MAAF ANDA HARUS TERSAMBUNG INTERNET DAHULU"
+            read
+            clear
           fi
           ;;
         7 )
+          clear
+          echo "terima kasih"
+          read
+          clear
           loop=2
       esac
     done
